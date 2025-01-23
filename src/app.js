9990cc06723a563fad05e8d1fd6d1d35b59c2108
RajWalke <rajwalke66@gmail.com>
2025-01-23 21:07:14 +0530

    Password is converted into the Hash Password

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 const express=require("express");
-
+const bcrypt=require("bcrypt");
 const app=express();
 const {connectDB}=require("./config/database");
 const employeInfo=require("./models/employeSchema");
@@ -24,18 +24,55 @@ app.use(express.json());
 
 app.post("/signup",async(req,res)=>{
     const data=req.body;
-   
+    const {firstName, lastName, email,password,age,gender,about}=data;
     // console.log(data);
     
     try{
         validation(data);
-        const employe=new employeInfo(data);
+        const hashPassword=await bcrypt.hash(password,10);
+        console.log(hashPassword);
+        const employe=new employeInfo({
+            firstName,
+            lastName,
+            email,
+            password:hashPassword,
+            age,
+            gender,
+            about
+        });
+
         await employe.save();
         res.send("Data is store");
     }catch(err){
         res.status(404).send("Error: "+err.message)
     }
     
+
+})
+
+app.post("/login",async(req,res)=>{
+    const userEmail=req.body.email;
+    const userPassword=req.body.password;
+
+    try{
+       const isEmail=await employeInfo.findOne({email:userEmail});
+       if(isEmail){
+        console.log(isEmail);
+        const {password}=isEmail;
+        const isvalide=await bcrypt.compare(userPassword,password);
+        if(isvalide){
+            res.send("Login Sucsssefully!!");
+        }else{
+            throw new Error("Password is Wrong");   
+        }
+        
+       }else{
+        throw new Error("EEmail is Wrong")
+       }
+
+    }catch(err){
+        res.status(404).send("Error: "+ err.message);
+    }
 
 })
 
